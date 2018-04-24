670097776388f1e7264b373300bcfdb4645e6432
imn5100 <imn5100_chu2@163.com>
2018-04-24 14:56:44 +0800

    spring-cloud-config client refresh

--- cloud-config-client/src/main/java/com/shaw/cloudtest/web/ConfigController.java
+++ cloud-config-client/src/main/java/com/shaw/cloudtest/web/ConfigController.java
@@ -2,10 +2,12 @@ package com.shaw.cloudtest.web;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigController {
 
     @Value("${myconfig.name}")
